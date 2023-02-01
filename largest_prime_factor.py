65b806e5a14531698902f8f6723efc7d6faf3dde
umutZT <umtozturk2003@gmail.com>
2023-02-01 10:45:02 +0300

    Create largest_prime_factor.py

---
+++ largest_prime_factor.py
@@ -0,0 +1,21 @@
+def find_prime(number):
+    while number % 2 == 0:
+        number = number //2
+    prime_num_can1 = (number //2) + 2
+    while prime_num_can1 > 2:
+        if number % prime_num_can1 == 0 :
+            if is_it_prime(prime_num_can1):
+                return prime_num_can1
+        prime_num_can1 -= 2
+
+def is_it_prime(number):   
+   for i in range(2,((number//2) + 1 )):
+       if (number % i) == 0:
+           return False
+   else:
+       return True
+
+
+number2 = 78451
+
+print(find_prime(number2))
